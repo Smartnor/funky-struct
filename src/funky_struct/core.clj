1fb7653785a44bc05d4c5e2a71e750c374a4d5eb
Andrew T. Flower <andrewtflower@gmail.com>
2016-11-21 22:43:02 -0500

    Add functions common to all of funky-struct

--- src/funky_struct/core.clj
+++ src/funky_struct/core.clj
@@ -1,7 +1,34 @@
-(ns funky-struct.core)
+(ns funky-struct.core
+  (:refer-clojure :rename {cons core-cons}
+                  :exclude [reverse]))
 
+(declare cons)
+
+; Protocol definitions
 (defprotocol Queue
   (is-empty? [q])
   (snoc [q x])
   (head [q])
   (tail [q]))
+
+; Private functions
+(defn- reverse'
+  [s r]
+  (if (empty? s)
+    r
+    (recur (rest s)
+           (cons (first s) r))))
+
+; Common functions
+(defn cons
+  [x r]
+  (lazy-seq (core-cons x r)))
+(defn ++
+  [s t]
+  (lazy-seq (if (empty? s)
+              t
+              (cons (first s)
+                    (++ (rest s) t)))))
+(defn reverse
+  [s]
+  (lazy-seq (reverse' s '())))
--- test/funky_struct/core_test.clj
+++ test/funky_struct/core_test.clj
@@ -1,3 +1,16 @@
 (ns funky-struct.core-test
+  (:refer-clojure :exclude [cons reverse])
   (:require [expectations :refer :all]
             [funky-struct.core :refer :all]))
+
+; Test common functions
+(expect '(:a :b :c :d)
+        (cons :a '(:b :c :d)))
+(expect '(:a :b :c :d)
+        (++ '(:a :b) '(:c :d)))
+(expect :a
+        (first (++ '(:a :b) '(:c :d))))
+(expect '(:d :c :b :a)
+        (reverse '(:a :b :c :d)))
+(expect :d
+        (first (reverse '(:a :b :c :d))))
